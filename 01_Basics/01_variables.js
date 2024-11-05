b0ebde8898fc71f07081f56595baff09f5816d64
Shrungashree2025 <shrungashreep@gmail.com>
2024-11-05 09:29:48 +0000

    Conversion of Datatypes

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 123
+let accountEmail="sp@sp.com"
+var accountPassword ="12345"
+accountCity = "BNG"
+
+
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
