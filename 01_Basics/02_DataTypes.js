8d6a787dda580797657ecd6b282794c4b136e639
Shrungashree2025 <shrungashreep@gmail.com>
2024-11-05 08:48:18 +0000

    DATATYPES

---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,24 @@
+"use Strict"
+
+console.log("SHRUNGASHREE")
+
+let name ="SHRUNGA"
+let age = 25
+let isLoggedIn = false
+let state;
+
+
+//number =>2 tpo 54
+//string 
+//bigint
+//booolean
+//null- standalone value
+//undefined---value not defined(Var created but nt assigned value)
+//symbol-uniqueness
+
+
+
+//Objects
+
+console.log(typeof null)
+//type of null----object
